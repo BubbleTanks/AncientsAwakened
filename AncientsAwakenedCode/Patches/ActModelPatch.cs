7d35afac62803f9e3b72afb896b5ca35e8309e60
SoytheProton <alttimelinemod@gmail.com>
2026-09-24 13:37:47 -0700

    Made Dark Spark add in extra nodes of each type.

--- AncientsAwakenedCode/Patches/ActModelPatch.cs
+++ AncientsAwakenedCode/Patches/ActModelPatch.cs
@@ -1,15 +1,19 @@
-﻿using AncientsAwakened.AncientsAwakenedCode.Relics.Gaster;
+﻿using System.Reflection;
+using AncientsAwakened.AncientsAwakenedCode.Relics.Gaster;
 using AncientsAwakened.AncientsAwakenedCode.UI;
 using HarmonyLib;
+using MegaCrit.Sts2.Core.Map;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Random;
 using MegaCrit.Sts2.Core.Runs;
+using MegaCrit.Sts2.Core.Unlocks;
 
 namespace AncientsAwakened.AncientsAwakenedCode.Patches;
 
 public static class ActModelPatch
 {
     [HarmonyPatch(typeof(ActModel), nameof(ActModel.GetNumberOfRooms))]
-    public class FilterForCombatPatch
+    public class GetNumberOfRoomsPatch
     {
         public static int Postfix(int __result)
         {
@@ -21,4 +25,53 @@ public static class ActModelPatch
             return __result + (int) relicCount * DarkSpark.AdditionalFloors;
         }
     }
+    
+    [HarmonyPatch]
+    public static class GetMapPointTypesPatch
+    {
+        [HarmonyTargetMethods]
+        public static IEnumerable<MethodBase> TargetMethods()
+        {
+            var abstractMethod = AccessTools.DeclaredMethod(
+                typeof(ActModel),
+                nameof(ActModel.GetMapPointTypes),
+                [typeof(Rng)]);
+
+            return AccessTools.AllTypes()
+                .Where(type =>
+                    type != typeof(ActModel) &&
+                    typeof(ActModel).IsAssignableFrom(type))
+                .Select(type => AccessTools.Method(
+                    type,
+                    nameof(ActModel.GetMapPointTypes),
+                    [typeof(Rng)]))
+                .Where(method =>
+                    method is not null &&
+                    !method.IsAbstract &&
+                    method.GetBaseDefinition() == abstractMethod)
+                .Distinct();
+        }
+
+        [HarmonyPostfix]
+        private static MapPointTypeCounts Postfix(
+            MapPointTypeCounts __result,
+            ActModel __instance)
+        {
+            var relicCount = RunManager.Instance.State?.Players.Sum(player => player.Relics.Count(r => r is DarkSpark));
+            if (relicCount is <= 0 or null)
+                return __result;
+            var newCounts = new CustomMapPointTypeCounts(__result, (int) relicCount);
+            Traverse.Create(newCounts).Field("<"+nameof(MapPointTypeCounts.NumOfShops)+">k__BackingField").SetValue(__result.NumOfShops + 1);
+            return newCounts;
+        }
+    }
+
+    private class CustomMapPointTypeCounts : MapPointTypeCounts
+    {
+        public CustomMapPointTypeCounts(MapPointTypeCounts typeCounts, int relicCount) : base(typeCounts.NumOfUnknowns + relicCount * 2, typeCounts.NumOfRests + relicCount)
+        {
+            NumOfElites = typeCounts.NumOfElites + relicCount;
+        }
+    }
+    
 }
